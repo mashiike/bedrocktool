e89cd4527702620104c421e2b3f206e8e1c92a24
mashiike <ikeda-masashi@kayac.com>
2024-09-17 12:44:55 +0900

    extra spec

--- tool/remote/spec.go
+++ tool/remote/spec.go
@@ -11,8 +11,37 @@ type Specification struct {
 	InputSchema json.RawMessage `json:"input_schema"`
 	// WorkerEndpoint of the tool.
 	WorkerEndpoint string `json:"worker_endpoint"`
-	// VerifyEndpoint of the tool. (optional) check for tool execution authorization.
-	VerifyEndpoint string `json:"verify_endpoint,omitempty"`
+	// Extra
+	Extra map[string]json.RawMessage `json:"-,omitempty"`
 }
 
 var DefaultSpecificationPath = "/.well-known/bedrock-tool-specification"
+
+func (s *Specification) UnmarshalJSON(data []byte) error {
+	type alias Specification
+	if err := json.Unmarshal(data, (*alias)(s)); err != nil {
+		return err
+	}
+	var extra map[string]json.RawMessage
+	if err := json.Unmarshal(data, &extra); err != nil {
+		return err
+	}
+	delete(extra, "name")
+	delete(extra, "description")
+	delete(extra, "input_schema")
+	delete(extra, "worker_endpoint")
+	s.Extra = extra
+	return nil
+}
+
+func (s *Specification) MarshalJSON() ([]byte, error) {
+	data := make(map[string]any, len(s.Extra)+4)
+	for k, v := range s.Extra {
+		data[k] = v
+	}
+	data["name"] = s.Name
+	data["description"] = s.Description
+	data["input_schema"] = s.InputSchema
+	data["worker_endpoint"] = s.WorkerEndpoint
+	return json.Marshal(data)
+}
